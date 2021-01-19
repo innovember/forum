485d52ca3c784b09384d200c7748ebe9bd468112
innovember <berikakhmed@gmail.com>
2021-01-19 20:12:31 +0600

    add Dockerfile

---
+++ api/deploy_docker.sh
@@ -0,0 +1,17 @@
+docker image build -f Dockerfile -t "forum" .
+
+echo "----------------------------------------------------------------"
+
+docker images
+
+echo "----------------------------------------------------------------"
+
+docker container run -p 9090:8181 --detach --name forumService forum
+
+echo "----------------------------------------------------------------"
+
+docker ps -a
+
+echo "----------------------------------------------------------------"
+
+docker exec -it forumService /bin/bash
