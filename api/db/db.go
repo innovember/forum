395fb9ed3dc3e792699326502cb6eaf19d96d330
innovember <berikakhmed@gmail.com>
2021-01-21 22:50:18 +0600

    add connection to database

---
+++ api/db/db.go
@@ -0,0 +1,29 @@
+package db
+
+import (
+	"database/sql"
+	config "github.com/innovember/forum/api/config"
+	_ "github.com/mattn/go-sqlite3"
+	"os"
+)
+
+var dbConn *sql.DB
+
+// get instance of db connection, and check db integrity with schema
+func GetDBInstance() (dbConn, err error) {
+	if _, err = os.Stat(config.DBPath); os.IsNotExist(err) {
+		if err = os.Mkdir(config.DBPath, 0755); err != nil {
+			return nil, err
+		}
+	}
+	if dbConn, err = sql.Open(config.DBDriver, config.DBPath+"/"+config.DBFileName); err != nil {
+		return nil, err
+	}
+	dbConn.SetMaxIdleConns(100)
+	if err = dbConn.Ping(); err != nil {
+		return nil, err
+	}
+	return dbConn, nil
+}
+
+func CheckDB(dbConn *sql.DB)
