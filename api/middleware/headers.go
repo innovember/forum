5c409c512b15946783a9556bb834e77562dba115
innovember <berikakhmed@gmail.com>
2021-01-27 18:54:06 +0600

    add backup func for cors

--- api/middleware/headers.go
+++ api/middleware/headers.go
@@ -19,3 +19,21 @@ func (mw *MiddlewareManager) SetHeaders(next http.HandlerFunc) http.HandlerFunc 
 		next(w, r)
 	}
 }
+
+// func (mw *MiddlewareManager) SetHeaders(next http.HandlerFunc) http.HandlerFunc {
+// 	return func(w http.ResponseWriter, r *http.Request) {
+// 		if origin := r.Header.Get("Origin"); origin != "" {
+// 			w.Header().Set("Access-Control-Allow-Origin", origin)
+// 			w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+// 			w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+// 			w.Header().Set("Access-Control-Allow-Credentials", "true")
+// 			// w.Header().Set("Access-Control-Allow-Origin", config.ClientURL)
+// 			if r.Method == "OPTIONS" {
+// 				w.WriteHeader(200)
+// 				return
+// 			}
+// 			w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+// 			next(w, r)
+// 		}
+// 	}
+// }
